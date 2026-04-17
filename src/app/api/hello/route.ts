1250f2a60597c35fda3b976866c64153823f4a1b
ido241117 <ido241117@users.noreply.github.com>
2026-04-17 13:18:39 +0700

    feat: add abc to hello API response

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ message: "hello sos railway" });
+  return NextResponse.json({ message: "hello sos railway abc" });
 }
